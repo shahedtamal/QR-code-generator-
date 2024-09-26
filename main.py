89b507b9aedd0fb7b8b7f57f3ac42ddddfc5c197
shahed alam <shahedtamal@gmail.com>
2024-09-26 23:21:54 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import qrcode
+
+def generate_qr_code(data, file_name='qrcode.png'):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(file_name)
+
+if __name__ == "__main__":
+    data_to_encode = "https://www.facebook.com"
+    generate_qr_code(data_to_encode)
